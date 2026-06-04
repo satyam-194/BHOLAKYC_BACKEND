1e231865577964e14d5963eff7e02bce0f72f0a3
awanish kumar yadav <aay14155@gmail.com>
2026-06-04 14:23:45 +0530

    fixed bugs

--- server.js
+++ server.js
@@ -68,7 +68,15 @@ app.use(cors({
 app.use(express.json({ limit: '128kb' }));
 app.use(express.urlencoded({ extended: true, limit: '20kb' }));
 
-app.use('/storage', express.static(STORAGE_ROOT));
+app.use('/storage', (req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin && (allowedOrigins.includes(origin) || isLocalDevOrigin(origin))) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Vary', 'Origin');
+  }
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+}, express.static(STORAGE_ROOT));
 
 app.get('/', (req, res) => {
   res.send('Backend is running 🚀');
